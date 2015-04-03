5a3edc02c19ae0eacc1e3430b36b9d1d6f7fe5e8
Chris Torstenson <c.torsday+github@gmail.com>
2015-04-03 11:42:22 -0700

    refactoring

--- ct_bil_spec.rb
+++ ct_bil_spec.rb
@@ -6,14 +6,14 @@ include CtBil
 describe CtBil do
   describe "count" do
     it "should work" do
-      expect(CtBil::kmer_count(str: "ACAACTATGCATACTATCGGGAACTATCCT", pattern: "ACTAT")).to eq(3)
-      expect(CtBil::kmer_count(str: "CGATATATCCATAG", pattern: "ATA")).to eq(3)
+      expect(kmer_count(str: "ACAACTATGCATACTATCGGGAACTATCCT", pattern: "ACTAT")).to eq(3)
+      expect(kmer_count(str: "CGATATATCCATAG", pattern: "ATA")).to eq(3)
     end
   end
 
   describe "most_frequent_kmers" do
     it "should work" do
-      expect(CtBil::most_frequent_kmers(str: "abc123xyz456abc123xyz456abc123xyz456", min_chunk_size: 4, max_chunk_size: 6)).to eq({3=>["abc1", "bc12", "c123", "123x", "23xy", "3xyz", "xyz4", "yz45", "z456", "abc12", "bc123", "c123x", "123xy", "23xyz", "3xyz4", "xyz45", "yz456", "abc123", "bc123x", "c123xy", "123xyz", "23xyz4", "3xyz45", "xyz456"]})
+      expect(most_frequent_kmers(str: "abc123xyz456abc123xyz456abc123xyz456", min_chunk_size: 4, max_chunk_size: 6)).to eq({3=>["abc1", "bc12", "c123", "123x", "23xy", "3xyz", "xyz4", "yz45", "z456", "abc12", "bc123", "c123x", "123xy", "23xyz", "3xyz4", "xyz45", "yz456", "abc123", "bc123x", "c123xy", "123xyz", "23xyz4", "3xyz45", "xyz456"]})
     end
   end
 
